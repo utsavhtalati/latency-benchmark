7af6cdcda9e2e1aa74a49653a4147cb1caff08c4
utsavhtalati <utsav.h.talati@vanderbilt.edu>
2025-03-24 11:58:28 -0500

    add: src/tcp_client & tcp_server

---
+++ src/tcp_client.py
@@ -0,0 +1,6 @@
+import socket
+import time
+
+# tcp_client.py
+# This script implements a simple TCP client for benchmarking latency.
+# It connects to a TCP server, sends a message, and measures the round-trip time.
---
+++ src/tcp_server.py
@@ -0,0 +1,7 @@
+import socket
+
+# tcp_server.py
+# This script implements a simple TCP server for benchmarking latency.
+# The server listens for incoming connections, receives data, and sends responses.
+
+
